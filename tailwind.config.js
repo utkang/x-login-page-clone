16c8bf2390076db9342fb957d67ecb6560b0355e
Utkan Genc <utkangencdev@gmail.com>
2023-08-22 21:33:21 +0300

    Adding Roboto font to project, and setting tailwind config for font, colors, and spacing.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "517px",
+        md: "1017px",
+        lg: "1280px",
+      },
+    },
+
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+
+      colors: {
+        'tw-white': 'rgb(255, 255, 255)',
+        'tw-black': 'rgb(0, 0, 0)',
+        'tw-blue': 'rgb(29, 155, 240)',
+        'tw-dark-grey': 'rgb(83, 100, 113)',
+      },
+
+      spacing: {
+        128: '32rem',
+      },
+
+    },
   },
   plugins: [],
 }
